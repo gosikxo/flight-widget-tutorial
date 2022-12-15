94f6f125c5b0a16aaeb36a4dc6462a7bcdbd04e9
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-12-15 13:27:50 +0100

    Fetch data to set flights in table body

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
 app.get('/flights', (req, res) => {
     const options = {
         url: `${process.env.URL}?page-size=5`,
--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import TableRow from './TableRow'
 
 export const TableBody = () => {
+  const [flights, setFlights] = useState(null)
+
+  const getFlights = () => {
+    fetch('http://localhost:8000/flights')
+      .then(response => response.json())
+      .then(flights => setFlights(Object.values(flights.data)))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => getFlights(), [])
+
+  console.log(flights)
+
   return (
-    <div></div>
+    <tbody>
+      {flights?.map((flight, _index) => (
+        <TableRow key={_index} flight={flights} />
+      ))}
+    </tbody>
   )
 }
